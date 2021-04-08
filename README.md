b597fd363d0745fae590f8f5f8c0076b293ba9c5
Adamek, Mateusz <Mateusz.Adamek@harman.com>
2021-04-08 10:48:58 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Hello Vizlib!
+
+## Prerequisites
+
+* docker 
+* docker-compose [https://docs.docker.com/compose/install/]
+
+## How to run?
+
+* Just execute: 
+  ```
+  chmod +x run.sh; ./run.sh
+  ```
---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: run.sh
+# 
+#         USAGE: ./run.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Mateusz Adamek (), mateusz.adamek@harman.com
+#       CREATED: 08.04.2021 10:38
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+docker-compose -f deploy/compose.yml
+
